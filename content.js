02056c2e51f529a9efd1a0ad12aca193ad34603e
Yunjong Jung <gandarf@gmail.com>
2025-01-07 13:35:33 +0900

    Adding fixed title and change keycodes.

--- content.js
+++ content.js
@@ -1,22 +1,46 @@
-const titleElement = document.getElementById('ai_cm_title');
-if (titleElement) {
-  titleElement.style.fontSize = '20px';
+const scrollAmount = 350; // 스크롤 거리 변수
+
+function handleScroll(direction) {
+  const top = direction === 'up' 
+    ? window.scrollY - scrollAmount 
+    : window.scrollY + scrollAmount;
+  window.scrollTo({ top, behavior: 'smooth' });
+}
+
+function handleNavigation(direction) {
+  const url = new URL(window.location.href);
+  const number = parseInt(url.searchParams.get('number'));
+
+  if (!isNaN(number)) { // number 변수 유효성 검사
+    const newNumber = direction === 'prev' ? number - 1 : number + 1;
+    url.searchParams.set('number', newNumber);
+    window.location.href = url.href;
+  };
 }
 
 document.addEventListener('keydown', function(event) {
-  if (event.key === 'j' || event.key === 'k') {
-    const url = new URL(window.location.href);
-    const number = parseInt(url.searchParams.get('number'));
-
-    if (event.key === 'k') {
-      url.searchParams.set('number', number - 1);
-    } else {
-      url.searchParams.set('number', number + 1);
-    }
-
-    window.location.href = url.href;
-  } else if (event.key === 'h') {
-    window.scrollTo({ top: window.scrollY - 300, behavior: 'smooth' });
-  } else if (event.key === 'l') {
-    window.scrollTo({ top: window.scrollY + 300, behavior: 'smooth' }); 
+  if (event.key === 's') {
+    handleNavigation('next');
+  } else if (event.key === 'd') {
+    handleNavigation('prev');
+  } else if (event.key === 'a') {
+    handleScroll('up');
+  } else if (event.key === 'f') {
+    handleScroll('down');
+  }
 });
+
+const titleElement = document.getElementById('ai_cm_title');
+
+if (titleElement) {
+  const originalStyle = titleElement.getAttribute('style');
+  titleElement.style.fontSize = '20px';
+  titleElement.style.position = 'fixed';
+  titleElement.style.top = '0'; 
+  titleElement.style.left = '0'; 
+  titleElement.style.zIndex = '9999'; 
+
+  window.addEventListener('beforeunload', () => {
+    titleElement.setAttribute('style', originalStyle);
+  }); 
+}
